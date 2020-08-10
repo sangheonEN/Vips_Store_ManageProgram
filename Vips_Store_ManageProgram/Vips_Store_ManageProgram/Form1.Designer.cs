6eca979b9fdc1a4d1e2cfc7f79fad33cc90f01e3
sangheonEN <jteks6@gmail.com>
2020-08-10 17:30:34 +0900

    Vips_Store_ManageProgram_20200810_Commit
    - 원하는 테이블에 정보를 저장하기 위한 변수 연동
    - 성인, 아동 인원수 +,- 구현
    - 회원 관리창 Form2 버튼 생성

--- Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.Designer.cs
+++ Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.Designer.cs
@@ -28,23 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.Customer_Manage = new System.Windows.Forms.Button();
             this.Table1 = new System.Windows.Forms.Button();
-            this.Adults = new System.Windows.Forms.Button();
-            this.Kids = new System.Windows.Forms.Button();
+            this.Adults_Plus = new System.Windows.Forms.Button();
+            this.Kids_Plus = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.Table2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Adult_Minus = new System.Windows.Forms.Button();
+            this.Kids_Minus = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Customer_Manage
             // 
-            this.button1.Location = new System.Drawing.Point(490, 488);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 31);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "회원 관리 창";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Customer_Manage.Location = new System.Drawing.Point(490, 488);
+            this.Customer_Manage.Name = "Customer_Manage";
+            this.Customer_Manage.Size = new System.Drawing.Size(111, 31);
+            this.Customer_Manage.TabIndex = 0;
+            this.Customer_Manage.Text = "회원 관리 창";
+            this.Customer_Manage.UseVisualStyleBackColor = true;
+            this.Customer_Manage.Click += new System.EventHandler(this.button1_Click);
             // 
             // Table1
             // 
@@ -56,69 +60,119 @@
             this.Table1.UseVisualStyleBackColor = true;
             this.Table1.Click += new System.EventHandler(this.Table1_Click);
             // 
-            // Adults
+            // Adults_Plus
             // 
-            this.Adults.Location = new System.Drawing.Point(312, 33);
-            this.Adults.Name = "Adults";
-            this.Adults.Size = new System.Drawing.Size(75, 23);
-            this.Adults.TabIndex = 2;
-            this.Adults.Text = "성인";
-            this.Adults.UseVisualStyleBackColor = true;
-            this.Adults.Click += new System.EventHandler(this.Adults_Click);
+            this.Adults_Plus.Location = new System.Drawing.Point(445, 191);
+            this.Adults_Plus.Name = "Adults_Plus";
+            this.Adults_Plus.Size = new System.Drawing.Size(75, 23);
+            this.Adults_Plus.TabIndex = 2;
+            this.Adults_Plus.Text = "성인 +1";
+            this.Adults_Plus.UseVisualStyleBackColor = true;
+            this.Adults_Plus.Click += new System.EventHandler(this.Adults_Click);
             // 
-            // Kids
+            // Kids_Plus
             // 
-            this.Kids.Location = new System.Drawing.Point(442, 33);
-            this.Kids.Name = "Kids";
-            this.Kids.Size = new System.Drawing.Size(75, 23);
-            this.Kids.TabIndex = 3;
-            this.Kids.Text = "아동";
-            this.Kids.UseVisualStyleBackColor = true;
-            this.Kids.Click += new System.EventHandler(this.Kids_Click);
+            this.Kids_Plus.Location = new System.Drawing.Point(526, 191);
+            this.Kids_Plus.Name = "Kids_Plus";
+            this.Kids_Plus.Size = new System.Drawing.Size(75, 23);
+            this.Kids_Plus.TabIndex = 3;
+            this.Kids_Plus.Text = "아동 +1";
+            this.Kids_Plus.UseVisualStyleBackColor = true;
+            this.Kids_Plus.Click += new System.EventHandler(this.Kids_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(312, 170);
+            this.listBox1.Location = new System.Drawing.Point(445, 40);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(147, 112);
+            this.listBox1.Size = new System.Drawing.Size(156, 124);
             this.listBox1.TabIndex = 4;
             // 
-            // button2
+            // Table2
             // 
-            this.button2.Location = new System.Drawing.Point(54, 191);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(198, 91);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Table2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Table2.Location = new System.Drawing.Point(54, 191);
+            this.Table2.Name = "Table2";
+            this.Table2.Size = new System.Drawing.Size(198, 91);
+            this.Table2.TabIndex = 6;
+            this.Table2.Text = "Table2";
+            this.Table2.UseVisualStyleBackColor = true;
+            this.Table2.Click += new System.EventHandler(this.Table2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(476, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "인원수 추가";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(389, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "원하는 테이블을 선택하세요.";
+            // 
+            // Adult_Minus
+            // 
+            this.Adult_Minus.Location = new System.Drawing.Point(445, 220);
+            this.Adult_Minus.Name = "Adult_Minus";
+            this.Adult_Minus.Size = new System.Drawing.Size(75, 23);
+            this.Adult_Minus.TabIndex = 9;
+            this.Adult_Minus.Text = "성인 -1";
+            this.Adult_Minus.UseVisualStyleBackColor = true;
+            this.Adult_Minus.Click += new System.EventHandler(this.Adult_Minus_Click);
+            // 
+            // Kids_Minus
+            // 
+            this.Kids_Minus.Location = new System.Drawing.Point(526, 220);
+            this.Kids_Minus.Name = "Kids_Minus";
+            this.Kids_Minus.Size = new System.Drawing.Size(75, 23);
+            this.Kids_Minus.TabIndex = 10;
+            this.Kids_Minus.Text = "아동 -1";
+            this.Kids_Minus.UseVisualStyleBackColor = true;
+            this.Kids_Minus.Click += new System.EventHandler(this.Kids_Minus_Click);
             // 
             // Form1_Table
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(619, 564);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Kids_Minus);
+            this.Controls.Add(this.Adult_Minus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Table2);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.Kids);
-            this.Controls.Add(this.Adults);
+            this.Controls.Add(this.Kids_Plus);
+            this.Controls.Add(this.Adults_Plus);
             this.Controls.Add(this.Table1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Customer_Manage);
             this.Name = "Form1_Table";
             this.Text = "Table 현황";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Customer_Manage;
         private System.Windows.Forms.Button Table1;
-        private System.Windows.Forms.Button Adults;
-        private System.Windows.Forms.Button Kids;
+        private System.Windows.Forms.Button Adults_Plus;
+        private System.Windows.Forms.Button Kids_Plus;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Table2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Adult_Minus;
+        private System.Windows.Forms.Button Kids_Minus;
     }
 }
 
--- Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.cs
+++ Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.cs
@@ -16,6 +16,7 @@ namespace Vips_Store_ManageProgram
         int count_kid = 0;
         int count_adult2 = 0;
         int count_kid2 = 0;
+
         public Form1_Table()
         {
             InitializeComponent();
@@ -29,18 +30,26 @@ namespace Vips_Store_ManageProgram
             }
         }
 
+        //회원 관리 프로그램 Form2로 이동
         private void button1_Click(object sender, EventArgs e)
         {
             Form2_Customer test = new Form2_Customer();
             test.Show();
         }
 
+        //Table_Button에 직접 Display되도록 설정
         private void Table1_Click(object sender, EventArgs e)
         {
             Table1.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult + "명\n" + "- 아동 " + count_kid + "명";       // Textbox 자체에서 display
             //MessageBox.Show("1번고객입장\n" + "입실 시간 : " + DateTime.Now.ToString() + "\n성인 " + count_adult + "명\n" + "\n아동" + count_kid + "명");  // Messagebox에서 display
         }
+        private void Table2_Click(object sender, EventArgs e)
+        {
+            Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
 
+        }
+
+        //원하는 ListBox index를 찾아가서 성인 인원 수 count ++
         private void Adults_Click(object sender, EventArgs e)
         {
             switch (listBox1.SelectedIndex)
@@ -51,28 +60,67 @@ namespace Vips_Store_ManageProgram
                     break;
                 case 1:
                     count_adult2++;
-                    button2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
-
+                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
                     break;
                 default:
                     break;
             }
-
-
-            if(listBox1.SelectedIndex == 0)
-            {
-
-                //MessageBox.Show( listBox1.SelectedItem.ToString());
-            }
-
-            //count_adult++;
-
-
         }
 
+        //원하는 ListBox index를 찾아가서 아동 인원 수 count ++
         private void Kids_Click(object sender, EventArgs e)
         {
-            //count_kid++;
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    count_kid++;
+                    Table1.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult + "명\n" + "- 아동 " + count_kid + "명";       // Textbox 자체에서 display
+                    break;
+                case 1:
+                    count_kid2++;
+                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        //원하는 ListBox index를 찾아가서 성인 인원 수 count --
+        private void Adult_Minus_Click(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    count_adult--;
+                    Table1.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult + "명\n" + "- 아동 " + count_kid + "명";       // Textbox 자체에서 display
+                    break;
+                case 1:
+                    count_adult2--;
+                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //원하는 ListBox index를 찾아가서 아동 인원 수 count --
+        private void Kids_Minus_Click(object sender, EventArgs e)
+        {
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    count_kid--;
+                    Table1.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult + "명\n" + "- 아동 " + count_kid + "명";       // Textbox 자체에서 display
+                    break;
+                case 1:
+                    count_kid2--;
+                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
