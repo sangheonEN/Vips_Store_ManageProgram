75d32f63113affc280e689290340e21515261b2a
sangheonEN <jteks6@gmail.com>
2020-08-12 10:20:27 +0900

    Vips_Store_ManageProgram_20200811_Commit
    Table 4개까지 추가

--- Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.Designer.cs
+++ Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1_Table));
             this.Customer_Manage = new System.Windows.Forms.Button();
             this.Table1 = new System.Windows.Forms.Button();
             this.Adults_Plus = new System.Windows.Forms.Button();
@@ -38,11 +39,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.Adult_Minus = new System.Windows.Forms.Button();
             this.Kids_Minus = new System.Windows.Forms.Button();
+            this.Table3 = new System.Windows.Forms.Button();
+            this.Table4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Customer_Manage
             // 
-            this.Customer_Manage.Location = new System.Drawing.Point(490, 488);
+            this.Customer_Manage.Location = new System.Drawing.Point(714, 506);
             this.Customer_Manage.Name = "Customer_Manage";
             this.Customer_Manage.Size = new System.Drawing.Size(111, 31);
             this.Customer_Manage.TabIndex = 0;
@@ -52,17 +57,19 @@
             // 
             // Table1
             // 
-            this.Table1.Location = new System.Drawing.Point(54, 77);
+            this.Table1.BackColor = System.Drawing.Color.White;
+            this.Table1.Font = new System.Drawing.Font("맑은 고딕 Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Table1.Location = new System.Drawing.Point(17, 95);
             this.Table1.Name = "Table1";
-            this.Table1.Size = new System.Drawing.Size(198, 93);
+            this.Table1.Size = new System.Drawing.Size(145, 75);
             this.Table1.TabIndex = 1;
             this.Table1.Text = "Table1";
-            this.Table1.UseVisualStyleBackColor = true;
+            this.Table1.UseVisualStyleBackColor = false;
             this.Table1.Click += new System.EventHandler(this.Table1_Click);
             // 
             // Adults_Plus
             // 
-            this.Adults_Plus.Location = new System.Drawing.Point(445, 191);
+            this.Adults_Plus.Location = new System.Drawing.Point(652, 191);
             this.Adults_Plus.Name = "Adults_Plus";
             this.Adults_Plus.Size = new System.Drawing.Size(75, 23);
             this.Adults_Plus.TabIndex = 2;
@@ -72,7 +79,7 @@
             // 
             // Kids_Plus
             // 
-            this.Kids_Plus.Location = new System.Drawing.Point(526, 191);
+            this.Kids_Plus.Location = new System.Drawing.Point(733, 191);
             this.Kids_Plus.Name = "Kids_Plus";
             this.Kids_Plus.Size = new System.Drawing.Size(75, 23);
             this.Kids_Plus.TabIndex = 3;
@@ -84,26 +91,28 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(445, 40);
+            this.listBox1.Location = new System.Drawing.Point(652, 40);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(156, 124);
             this.listBox1.TabIndex = 4;
             // 
             // Table2
             // 
-            this.Table2.Location = new System.Drawing.Point(54, 191);
+            this.Table2.BackColor = System.Drawing.Color.White;
+            this.Table2.Font = new System.Drawing.Font("맑은 고딕 Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Table2.Location = new System.Drawing.Point(169, 96);
             this.Table2.Name = "Table2";
-            this.Table2.Size = new System.Drawing.Size(198, 91);
+            this.Table2.Size = new System.Drawing.Size(145, 75);
             this.Table2.TabIndex = 6;
             this.Table2.Text = "Table2";
-            this.Table2.UseVisualStyleBackColor = true;
+            this.Table2.UseVisualStyleBackColor = false;
             this.Table2.Click += new System.EventHandler(this.Table2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(476, 167);
+            this.label1.Location = new System.Drawing.Point(683, 167);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 21);
             this.label1.TabIndex = 7;
@@ -113,7 +122,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(389, 9);
+            this.label2.Location = new System.Drawing.Point(596, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(218, 21);
             this.label2.TabIndex = 8;
@@ -121,7 +130,7 @@
             // 
             // Adult_Minus
             // 
-            this.Adult_Minus.Location = new System.Drawing.Point(445, 220);
+            this.Adult_Minus.Location = new System.Drawing.Point(652, 220);
             this.Adult_Minus.Name = "Adult_Minus";
             this.Adult_Minus.Size = new System.Drawing.Size(75, 23);
             this.Adult_Minus.TabIndex = 9;
@@ -131,7 +140,7 @@
             // 
             // Kids_Minus
             // 
-            this.Kids_Minus.Location = new System.Drawing.Point(526, 220);
+            this.Kids_Minus.Location = new System.Drawing.Point(733, 220);
             this.Kids_Minus.Name = "Kids_Minus";
             this.Kids_Minus.Size = new System.Drawing.Size(75, 23);
             this.Kids_Minus.TabIndex = 10;
@@ -139,23 +148,61 @@
             this.Kids_Minus.UseVisualStyleBackColor = true;
             this.Kids_Minus.Click += new System.EventHandler(this.Kids_Minus_Click);
             // 
+            // Table3
+            // 
+            this.Table3.BackColor = System.Drawing.Color.White;
+            this.Table3.Location = new System.Drawing.Point(415, 252);
+            this.Table3.Name = "Table3";
+            this.Table3.Size = new System.Drawing.Size(198, 85);
+            this.Table3.TabIndex = 11;
+            this.Table3.Text = "Table3";
+            this.Table3.UseVisualStyleBackColor = false;
+            this.Table3.Click += new System.EventHandler(this.Table3_Click);
+            // 
+            // Table4
+            // 
+            this.Table4.BackColor = System.Drawing.Color.White;
+            this.Table4.Location = new System.Drawing.Point(415, 363);
+            this.Table4.Name = "Table4";
+            this.Table4.Size = new System.Drawing.Size(198, 82);
+            this.Table4.TabIndex = 12;
+            this.Table4.Text = "Table4";
+            this.Table4.UseVisualStyleBackColor = false;
+            this.Table4.Click += new System.EventHandler(this.Table4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(335, 549);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1_Table
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 564);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(846, 549);
+            this.Controls.Add(this.Table2);
+            this.Controls.Add(this.Table1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Table4);
+            this.Controls.Add(this.Table3);
             this.Controls.Add(this.Kids_Minus);
             this.Controls.Add(this.Adult_Minus);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Table2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.Kids_Plus);
             this.Controls.Add(this.Adults_Plus);
-            this.Controls.Add(this.Table1);
             this.Controls.Add(this.Customer_Manage);
             this.Name = "Form1_Table";
             this.Text = "Table 현황";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,6 +220,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Adult_Minus;
         private System.Windows.Forms.Button Kids_Minus;
+        private System.Windows.Forms.Button Table3;
+        private System.Windows.Forms.Button Table4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.cs
+++ Vips_Store_ManageProgram/Vips_Store_ManageProgram/Form1.cs
@@ -13,22 +13,28 @@ namespace Vips_Store_ManageProgram
     public partial class Form1_Table : Form
     {
         int count_adult = 0;
-        int count_kid = 0;
         int count_adult2 = 0;
+        int count_adult3 = 0;
+        int count_adult4 = 0;
+        int count_kid = 0;
         int count_kid2 = 0;
+        int count_kid3 = 0;
+        int count_kid4 = 0;
 
         public Form1_Table()
         {
             InitializeComponent();
 
             //List_Box Table List 항목 저장
-            string[] Table_List = { "Table1", "Table2", "Table3", "Table4", "Table5", "Table6", "Table7", "Table8", "Table9", "Table10", "Table11", "Table12" };
+            string[] Table_List = { "Table1", "Table2", "Table3", "Table4"};
 
             foreach (var item in Table_List)
             {
                 listBox1.Items.Add(item);
             }
         }
+
+
 
         //회원 관리 프로그램 Form2로 이동
         private void button1_Click(object sender, EventArgs e)
@@ -46,7 +52,14 @@ namespace Vips_Store_ManageProgram
         private void Table2_Click(object sender, EventArgs e)
         {
             Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
-
+        }
+        private void Table3_Click(object sender, EventArgs e)
+        {
+            Table3.Text = "- 3번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult3 + "명\n" + "- 아동 " + count_kid3 + "명";       // Textbox 자체에서 display
+        }
+        private void Table4_Click(object sender, EventArgs e)
+        {
+            Table3.Text = "- 4번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult4 + "명\n" + "- 아동 " + count_kid4 + "명";       // Textbox 자체에서 display
         }
 
         //원하는 ListBox index를 찾아가서 성인 인원 수 count ++
@@ -60,7 +73,15 @@ namespace Vips_Store_ManageProgram
                     break;
                 case 1:
                     count_adult2++;
-                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+                case 2:
+                    count_adult3++;
+                    Table3.Text = "- 3번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult3 + "명\n" + "- 아동 " + count_kid3 + "명";       // Textbox 자체에서 display
+                    break;
+                case 3:
+                    count_adult4++;
+                    Table4.Text = "- 4번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult4 + "명\n" + "- 아동 " + count_kid4 + "명";       // Textbox 자체에서 display
                     break;
                 default:
                     break;
@@ -78,9 +99,16 @@ namespace Vips_Store_ManageProgram
                     break;
                 case 1:
                     count_kid2++;
-                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
                     break;
-
+                case 2:
+                    count_kid3++;
+                    Table3.Text = "- 3번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult3 + "명\n" + "- 아동 " + count_kid3 + "명";       // Textbox 자체에서 display
+                    break;
+                case 3:
+                    count_kid4++;
+                    Table4.Text = "- 4번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult4 + "명\n" + "- 아동 " + count_kid4 + "명";       // Textbox 자체에서 display
+                    break;
                 default:
                     break;
             }
@@ -97,7 +125,15 @@ namespace Vips_Store_ManageProgram
                     break;
                 case 1:
                     count_adult2--;
-                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+                case 2:
+                    count_adult3--;
+                    Table3.Text = "- 3번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult3 + "명\n" + "- 아동 " + count_kid3 + "명";       // Textbox 자체에서 display
+                    break;
+                case 3:
+                    count_adult4--;
+                    Table4.Text = "- 4번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult4 + "명\n" + "- 아동 " + count_kid4 + "명";       // Textbox 자체에서 display
                     break;
                 default:
                     break;
@@ -115,7 +151,15 @@ namespace Vips_Store_ManageProgram
                     break;
                 case 1:
                     count_kid2--;
-                    Table2.Text = "- 1번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    Table2.Text = "- 2번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult2 + "명\n" + "- 아동 " + count_kid2 + "명";       // Textbox 자체에서 display
+                    break;
+                case 2:
+                    count_kid3--;
+                    Table3.Text = "- 3번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult3 + "명\n" + "- 아동 " + count_kid3 + "명";       // Textbox 자체에서 display
+                    break;
+                case 3:
+                    count_kid4--;
+                    Table4.Text = "- 4번 Table \n" + "- 입실 시간 \n" + DateTime.Now.ToString() + "\n- 성인 " + count_adult4 + "명\n" + "- 아동 " + count_kid4 + "명";       // Textbox 자체에서 display
                     break;
 
                 default:
@@ -124,3 +168,5 @@ namespace Vips_Store_ManageProgram
         }
     }
 }
+
+
